80010a6c967db46557485ba1653a8455be05bc15
MilitoNathan <nathan.milito@efipeek.com>
2025-06-27 13:27:03 +0200

    fix(typo): Fixed typo in controller name

--- src/routes/eventregistration.routes.js
+++ src/routes/eventregistration.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/eventRegistration.controller');
+const controller = require('../controllers/eventregistration.controller');
 const { auth } = require('../middlewares/auth.middleware');
 
 router.get('/user', controller.getUserRegistrations);
